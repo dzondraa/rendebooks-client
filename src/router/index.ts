9f5ff8e9806061fa8884b18a036c218b3e6209f4
dzondraa <djole.nic@gmail.com>
2020-03-28 21:16:27 +0100

    Sidebar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Shop from '../views/Shop.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +13,9 @@ const routes = [
 	},
 	{
     path: '/shop',
-		name: 'Shop',
-		component: Home
-  },
+    name: 'Shop',
+    component: Shop
+	},
   {
     path: '/about',
     name: 'About',
---
+++ src/views/Shop.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+		<Sidebar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Sidebar from '@/components/shared/navigation/Sidebar'
+
+export default {
+  name: 'Shop',
+  components: { Sidebar }
+}
+</script>
